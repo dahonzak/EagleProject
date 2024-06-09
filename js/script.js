8a8f207f7c2d863eafcdff4bc32204dac4f7fba8
Dominik Honzak <108998079+dahonzak@users.noreply.github.com>
2024-06-09 21:03:57 +0000

    messed up colors

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const basic = {
   maps:null,
   colors:["white","yellow","orange","beige","brown","green","red","blue"],
   colordef:["Easy","Normal","Intermediate","Intermediate Long","Advanced","Advanced Long","Expert","Expert Long"],
-  colorhue:["rgb(255,255,255)","rgb(247, 237, 47)","rgb(255, 153, 0)","rgb(92, 63, 34)","rgb(19, 191, 42)","rgb(209, 15, 15)","rgb(46, 125, 209)"],
+  colorhue:["rgb(255,255,255)","rgb(247, 237, 47)","rgb(255, 153, 0)","rgb(117, 107, 75)","rgb(92, 63, 34)","rgb(19, 191, 42)","rgb(209, 15, 15)","rgb(46, 125, 209)"],
   navcon:true,
   timer:null,
   hotcold: false,
